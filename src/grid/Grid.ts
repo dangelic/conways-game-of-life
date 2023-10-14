bb928c4c90a076af1ed24bd23e2602445bbadbb2
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-10-15 00:10:07 +0200

    chore(grid): remove comments

--- src/grid/Grid.ts
+++ src/grid/Grid.ts
@@ -8,10 +8,10 @@ export class Grid {
     private margin: number;
 
     constructor() {
-      this.margin = 10; // Adjust this value to set the margin
+      this.margin = 10; 
       this.canvas = document.createElement('canvas');
-      this.canvas.width = 800 + 2 * this.margin; // Add margin to the width
-      this.canvas.height = 600 + 2 * this.margin; // Add margin to the height
+      this.canvas.width = 800 + 2 * this.margin; 
+      this.canvas.height = 600 + 2 * this.margin; 
       this.canvas.style.position = 'absolute';
       this.canvas.style.left = '50%';
       this.canvas.style.top = '50%';
@@ -19,8 +19,8 @@ export class Grid {
       document.body.appendChild(this.canvas);
       this.ctx = this.canvas.getContext('2d');
       this.cellSize = 20;
-      this.numRows = (this.canvas.height - 2 * this.margin) / this.cellSize; // Adjust for margin
-      this.numCols = (this.canvas.width - 2 * this.margin) / this.cellSize; // Adjust for margin
+      this.numRows = (this.canvas.height - 2 * this.margin) / this.cellSize; 
+      this.numCols = (this.canvas.width - 2 * this.margin) / this.cellSize;  
       this.initGrid();
       this.drawGrid();
       this.addClickListener();
@@ -37,16 +37,16 @@ export class Grid {
       for (let x = this.margin; x <= this.canvas.width - this.margin; x += this.cellSize) {
         const pixelX = Math.round(x);
         this.ctx.beginPath();
-        this.ctx.moveTo(pixelX, this.margin); // Adjust for margin
-        this.ctx.lineTo(pixelX, this.canvas.height - this.margin); // Adjust for margin
+        this.ctx.moveTo(pixelX, this.margin); 
+        this.ctx.lineTo(pixelX, this.canvas.height - this.margin); 
         this.ctx.stroke();
       }
 
       for (let y = this.margin; y <= this.canvas.height - this.margin; y += this.cellSize) {
         const pixelY = Math.round(y);
         this.ctx.beginPath();
-        this.ctx.moveTo(this.margin, pixelY); // Adjust for margin
-        this.ctx.lineTo(this.canvas.width - this.margin, pixelY); // Adjust for margin
+        this.ctx.moveTo(this.margin, pixelY); 
+        this.ctx.lineTo(this.canvas.width - this.margin, pixelY); 
         this.ctx.stroke();
       }
 
@@ -55,8 +55,8 @@ export class Grid {
           if (this.grid[i][j]) {
             this.ctx.fillStyle = 'black';
             this.ctx.fillRect(
-              j * this.cellSize + this.margin, // Adjust for margin
-              i * this.cellSize + this.margin, // Adjust for margin
+              j * this.cellSize + this.margin,  
+              i * this.cellSize + this.margin,  
               this.cellSize,
               this.cellSize
             );
@@ -70,8 +70,8 @@ export class Grid {
           const rect = this.canvas.getBoundingClientRect();
           const x = e.clientX - rect.left;
           const y = e.clientY - rect.top;
-          const cellX = Math.floor((x - this.margin) / this.cellSize); // Adjust for margin
-          const cellY = Math.floor((y - this.margin) / this.cellSize); // Adjust for margin
+          const cellX = Math.floor((x - this.margin) / this.cellSize);  
+          const cellY = Math.floor((y - this.margin) / this.cellSize);  
           this.grid[cellY][cellX] = !this.grid[cellY][cellX]; // Toggle the cell state
           this.drawGrid();
         });
