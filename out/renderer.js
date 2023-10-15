1144d1bc0d588a99dce9c5fc2f64ec6370e24dad
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-10-15 02:28:12 +0200

    feat(gui): add slider to adjust spawn chance

--- out/renderer.js
+++ out/renderer.js
@@ -2,14 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Grid_1 = require("./grid/Grid");
 const RandomSeed_1 = require("./seed/RandomSeed");
+let randomSpawnChance = 0.2; // Initial value
 window.onload = () => {
-    // TODO: Outsource
+    // Create a new Grid instance
+    const grid = new Grid_1.Grid();
+    // Create a slider to adjust randomSpawnChance
+    const slider = document.createElement('input');
+    slider.type = 'range';
+    slider.min = '0.05';
+    slider.max = '0.5';
+    slider.step = '0.01';
+    slider.value = randomSpawnChance.toString();
+    // Update randomSpawnChance when the slider value changes
+    slider.addEventListener('input', () => {
+        randomSpawnChance = parseFloat(slider.value);
+    });
+    // Create a button to randomize Generation 0
     const button = document.createElement('button');
     button.textContent = 'Randomize Gen 0';
     button.addEventListener('click', () => {
-        let randomSeed = new RandomSeed_1.RandomSeed(0.2);
+        const randomSeed = new RandomSeed_1.RandomSeed(randomSpawnChance);
         randomSeed.seedGenerationZero(grid);
     });
+    // Append the slider and button to the document
+    document.body.appendChild(slider);
     document.body.appendChild(button);
-    const grid = new Grid_1.Grid(); // Create a new Grid instance
 };
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,17 +1,34 @@
 import { Grid } from './grid/Grid';
 import { RandomSeed } from './seed/RandomSeed';
 
+let randomSpawnChance = 0.2; // Initial value
 
 window.onload = () => {
-    // TODO: Outsource
+    // Create a new Grid instance
+    const grid = new Grid();
+
+    // Create a slider to adjust randomSpawnChance
+    const slider = document.createElement('input');
+    slider.type = 'range';
+    slider.min = '0.05';
+    slider.max = '0.5';
+    slider.step = '0.01';
+    slider.value = randomSpawnChance.toString();
+
+    // Update randomSpawnChance when the slider value changes
+    slider.addEventListener('input', () => {
+        randomSpawnChance = parseFloat(slider.value);
+    });
+
+    // Create a button to randomize Generation 0
     const button = document.createElement('button');
     button.textContent = 'Randomize Gen 0';
     button.addEventListener('click', () => {
-        let randomSeed  = new RandomSeed(0.2) 
-        randomSeed.seedGenerationZero(grid)
+        const randomSeed = new RandomSeed(randomSpawnChance);
+        randomSeed.seedGenerationZero(grid);
     });
+
+    // Append the slider and button to the document
+    document.body.appendChild(slider);
     document.body.appendChild(button);
-
-
-  const grid = new Grid(); // Create a new Grid instance
 };
